b7cc953937f615032ffd355fa3be5c35b9697941
lee203333 <lieileen402@gmail.com>
2023-07-18 22:08:47 +0900

    delete fonts

--- css/randcolor.js
+++
@@ -1,6 +0,0 @@
-jQuery(function($){
-	for(var i=0 ; i < $(".random_colors").length; i++){
-		var f_color = "#"+(parseInt(Math.random()*0xffffff)).toString(16);
-		$(".random_colors").eq(i).css("color",f_color);
-	}
-});
